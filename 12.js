fffd42f1f67ded1ea2992e49236d2aa62a015934
smselimmuna102706 <111196494+smselimmuna102706@users.noreply.github.com>
2022-09-08 23:34:51 +0600

    Farenhite-Celcius-Calculator

---
+++ 12.js
@@ -0,0 +1,19 @@
+
+
+//  farenhite to celsius convertor 
+
+// var farn = parseFloat(prompt("Enter the farenhite :")) ;
+
+// var cels = (farn-32) * (5/9);
+
+// document.write("Celcius = " + cels +"</br>");
+
+
+//  celsius to farenhite convertor 
+
+var cels = parseFloat(prompt("Enter the celsius :")) ;
+
+var farn = (cels*(5/9)) + 32;
+
+document.write("Farenhite = " + farn);
+
